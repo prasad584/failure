f541c141681f1c7adc23e03f04a9b4917d4834ab
Prasad <27636876-prasad584@users.noreply.gitlab.com>
2025-10-05 12:14:55 +0530

    fail

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import app from "./index.js";
 
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
